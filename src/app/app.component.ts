fc21092d82db7fd3ce7e61080ef060acdb950f14
NkazimuloMvundla <nkazimulojudgement@gmail.com>
2021-01-21 14:44:02 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = "my-dream-app";
+  title = "Quiz App By Judge";
   name = "Judge";
 }
